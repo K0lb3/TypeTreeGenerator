4e1e861271a01e16e182a93b2a817a0ce41321df
7aGiven <fredrickareichert@gmail.com>
2024-12-05 13:44:45 +0800

    multi class

--- TypeTreeGenerator/Generator.cs
+++ TypeTreeGenerator/Generator.cs
@@ -15,14 +15,20 @@ namespace Generator
             this.assemblyLoader.Loaded = true;
         }
 
-        public IEnumerable<TypeDefinition> getTypeDefs(string m_AssemblyName, String m_ClassName, String m_Namespace)
+        public IEnumerable<TypeDefinition> getTypeDefs(string m_AssemblyName, List<String> m_ClassNames, String m_Namespace)
         {
-            IEnumerable<TypeDefinition> typeDefs = (m_ClassName.Length > 0) ?
-                new List<TypeDefinition>()
+            if (m_ClassNames.Count > 0)
+            {
+                List<TypeDefinition> typeDefs = new List<TypeDefinition>();
+                foreach (String m_ClassName in m_ClassNames)
                 {
-                    assemblyLoader.GetTypeDefinition(m_AssemblyName, string.IsNullOrEmpty(m_Namespace) ? m_ClassName : $"{m_Namespace}.{m_ClassName}")
-                } : assemblyLoader.GetTypeDefinitions(m_AssemblyName);
-            return typeDefs;
+                    typeDefs.Add(assemblyLoader.GetTypeDefinition(m_AssemblyName, string.IsNullOrEmpty(m_Namespace) ? m_ClassName : $"{m_Namespace}.{m_ClassName}"));
+                }
+                return typeDefs;
+            } else
+            {
+                return assemblyLoader.GetTypeDefinitions(m_AssemblyName);
+            }
         }
 
         public List<TypeTreeNode> convertToTypeTreeNodes(TypeDefinition typeDef, int[] version)
